0055b2e37486e1769026ce8a3895c0ce556abf8d
yoga16aryawan <yoga16aryawan@gmail.com>
2022-03-03 15:17:02 +0800

    tampilan halaman login page

---
+++ src/resources/views/layout/master.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="css/bulma.min.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>
