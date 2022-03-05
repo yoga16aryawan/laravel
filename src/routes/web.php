b50d26e6ac06c2f39df8e8a2e763314e48bbd22c
yoga16aryawan <yoga16aryawan@gmail.com>
2022-03-05 11:38:29 +0800

    coba view melalui controller

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Login;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function () {
-    return "halaman login";
-});
+Route::get('login', [Login::class, 'index']);
